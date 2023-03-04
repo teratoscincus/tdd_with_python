c6bc3e50ce1531e1fce4a63af34b37fc110b3c32
Max <teratoscincus@hotmail.com>
2023-03-04 14:55:31 +0100

    Specify functional test and use unittest

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-installation_success = "The install worked successfully! Congratulations!"
-assert installation_success in browser.title
-print("TEST: OK")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # User visits homepage.
+        self.browser.get("http://localhost:8000")
+
+        # Check that "To-Do" is in the page title.
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        # User is invited to make an entry.
+
+        # User enters "Buy tomatoes".
+
+        # On submission, the page updates and lists:
+        # "1: Buy tomatoes" as an item in a to-do list.
+
+        # There is still a textbox inviting the user to make another entry.
+
+        # User enters "Make tomato sauce"
+
+        # The page updates again, and now shows both items in the to-do list.
+
+        # Check that a unique URL for the user has been generated.
+
+        # Visit URL and ensure the users to-do list is still there.
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
