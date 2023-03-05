032bf3c885854dbb63b82a4a9fa365156ff48814
Max <teratoscincus@hotmail.com>
2023-03-05 20:49:05 +0100

    Refactor html response from view to html file
    Return a render() function with the newly created index.html passed to
    it as an argument, and move the HTML to said file.

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(b"<html><title>To-Do lists</title></html>")
+    return render(request, "lists/index.html")
