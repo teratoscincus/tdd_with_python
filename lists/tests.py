4fa490db0065c3a40673356393c37f2567433dcc
Max <teratoscincus@hotmail.com>
2023-03-05 23:46:20 +0100

    Add csrf_token to template
    Ignore csrf token when comparing templates.

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
@@ -7,6 +9,16 @@ from . import views
 
 
 class HomePageTest(TestCase):
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r"<input[^>]+csrfmiddlewaretoken[^>]+>"
+        return re.sub(csrf_regex, "", html_code)
+
+    def assertHTMLEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1), self.remove_csrf(html_code2)
+        )
+
     def test_root_url_resolve_to_index_view(self):
         found = resolve("/")
         self.assertEqual(found.func, views.index)
@@ -15,4 +27,4 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = views.index(request)
         expected_html = render_to_string("lists/index.html")
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertHTMLEqualExceptCSRF(response.content.decode(), expected_html)
