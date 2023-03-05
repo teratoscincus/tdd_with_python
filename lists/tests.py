4e94a97fbfe16b014b88f1d2047802c1ce4918bf
Max <teratoscincus@hotmail.com>
2023-03-05 12:51:47 +0100

    Index view return minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 
 from . import views
 
@@ -8,3 +9,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_index_view(self):
         found = resolve("/")
         self.assertEqual(found.func, views.index)
+
+    def test_index_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = views.index(request)
+        self.assertTrue(response.content.startswith(b"<html>"))
+        self.assertIn(b"<title>To-Do lists</title>", response.content)
+        self.assertTrue(response.content.endswith(b"</html>"))
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def index():
-    pass
+def index(request):
+    return HttpResponse(b"<html><title>To-Do lists</title></html>")
