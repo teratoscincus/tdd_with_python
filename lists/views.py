62c5244d0011a8a552bcb8f82d805f8945e256d5
Max <teratoscincus@hotmail.com>
2023-03-06 09:18:55 +0100

    List item submitted with POST request

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "lists/index.html")
+    return render(
+        request,
+        "lists/index.html",
+        context={"new_item_text": request.POST.get("item_text", "")},
+    )
