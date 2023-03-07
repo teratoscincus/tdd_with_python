7bc9b1df9181ce54fb1bf49096fd80a34d682976
Max <teratoscincus@hotmail.com>
2023-03-07 14:52:06 +0100

    Test for users having unique lists and urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # User visits homepage.
+        # User1 visits homepage.
         self.browser.get(self.live_server_url)
 
         # Check that "To-Do" is in the page title and header.
@@ -30,11 +30,11 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
-        # User is invited to make an entry.
+        # User1 is invited to make an entry.
         inputbox = self.browser.find_element(By.ID, "new_item")
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
-        # User enters "Buy tomatoes".
+        # User1 enters "Buy tomatoes".
         inputbox.send_keys("Buy tomatoes")
 
         # On submission by pressing Enter, the page updates and lists:
@@ -42,10 +42,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)  # Await page update after submit
 
+        # User1 get a unique URL for their list.
+        user1_list_url = self.browser.current_url
+        self.assertRegex(user1_list_url, "/lists/.+")
+
         self.assert_row_text_in_list_table("1: Buy tomatoes")
 
-        # There is still a textbox inviting the user to make another entry.
-        # User enters "Make tomato sauce"
+        # There is still a textbox inviting the user1 to make another entry.
+        # User1 enters "Make tomato sauce"
         inputbox = self.browser.find_element(By.ID, "new_item")
         inputbox.send_keys("Make tomato sauce")
         inputbox.send_keys(Keys.ENTER)
@@ -55,8 +59,33 @@ class NewVisitorTest(LiveServerTestCase):
         self.assert_row_text_in_list_table("1: Buy tomatoes")
         self.assert_row_text_in_list_table("2: Make tomato sauce")
 
-        # Check that a unique URL for the users list has been generated.
+        # A new user checks out the site.
+        ## Using a new browser session to ensure no info or cookies are shared.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # User2 visits the site.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy tomatoes", page_text)
+        self.assertNotIn("Make tomato sauce", page_text)
+
+        # User2 starts a new list by entering a new item.
+        inputbox = self.browser.find_element(By.ID, "new_item")
+        inputbox.send_keys("Write a test that fails")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # User2 get their own unique URL.
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, "/lists/.+")
+        self.assertNotEqual(user1_list_url, user2_list_url)
+
+        # User2 can only see their own list.
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy tomatoes", page_text)
+        self.assertIn("Write a test that fails", page_text)
+
+        # Both users leave the web site.
 
         self.fail("Test not fully implemented - Finish writing the test!")
-
-        # Visit URL and ensure the users to-do list is still there.
