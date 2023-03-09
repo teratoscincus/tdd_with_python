f5f979607c64679539daea16e51213b47b99a581
Max <teratoscincus@hotmail.com>
2023-03-09 14:27:16 +0100

    Reduce duplication in URLs for lists

--- lists/urls.py
+++ lists/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 
-from .views import index, view_list, new_list, add_item
+from .views import view_list, new_list, add_item
 
 app_name = "lists"
 urlpatterns = [
-    path("", index, name="index"),
-    path("lists/<int:pk>/", view_list, name="view_list"),
-    path("lists/<int:pk>/add-item", add_item, name="add_item"),
-    re_path(r"^lists/new$", new_list, name="new_list"),
+    path("<int:pk>/", view_list, name="view_list"),
+    path("<int:pk>/add-item", add_item, name="add_item"),
+    path("new", new_list, name="new_list"),
 ]
--- superlists/urls.py
+++ superlists/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from lists import views as list_views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("lists.urls")),
+    path("", list_views.index, name="index"),
+    path("lists/", include("lists.urls")),
 ]
